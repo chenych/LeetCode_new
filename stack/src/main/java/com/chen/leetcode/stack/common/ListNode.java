6c8d9911706e64058aac8aa358fcfa8c456a3468
冲达 <yuchong.cyc@antfin.com>
2020-06-30 11:36:40 +0800

    stack_20200630

---
+++ stack/src/main/java/com/chen/leetcode/stack/common/ListNode.java
@@ -0,0 +1,26 @@
+package com.chen.leetcode.stack.common;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x, ListNode next) {
+        val = x;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
